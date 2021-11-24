edd455e4edcb8833d28a66e8075acb2bf2c9ea01
Yudai Hayashi <yudai0913haya@gmail.com>
2021-11-24 14:33:49 +0900

    Create new endpoints

--- src/lib.rs
+++ src/lib.rs
@@ -48,15 +48,99 @@ pub extern "C" fn set_clock(id: c_short, clock_time: c_int, sel: c_uchar) -> c_s
 /// * `type1` - input range of CH1
 /// * `type2` - input range of CH2
 #[no_mangle]
-pub extern "C" fn input_set(id: i32, type1: u8, type2: u8) {
+pub extern "C" fn input_set(id: c_short, type1: u8, type2: u8) -> c_short {
+    let err: c_short;
     unsafe {
-        let err = operation::TUSB0216AD_Input_Set(id, type1, type2);
+        err = operation::TUSB0216AD_Input_Set(id, type1, type2);
         helper::parse_error(err, "TUSB0216AD_Input_Set");
     }
+
+    err
 }
 
 #[no_mangle]
-pub extern "C" fn run(id: i32, seconds: u64) {
+pub extern "C" fn input_check(id: c_short) -> i32 {
+    let mut type1: u8 = 0;
+    let mut type2: u8 = 0;
+    // let l_ptr = &mut length as *mut u32;
+    let type1_ptr = &mut type1 as *mut u8;
+    let type2_ptr = &mut type2 as *mut u8;
+    unsafe {
+        let err = operation::TUSB0216AD_Input_Check(id, type1_ptr, type2_ptr);
+        helper::parse_error(err, "TUSB0216AD_Input_Check");
+    }
+
+    return type1 as i32;
+}
+
+#[no_mangle]
+pub extern "C" fn start(id: c_short, ch: u8, prelen: i32, trig_type: u8, trig_ch: u8) -> c_short {
+    let err: c_short;
+    unsafe {
+        err = operation::TUSB0216AD_Start(id, ch, prelen, trig_type, trig_ch);
+        helper::parse_error(err, "TUSB0216AD_Start");
+    }
+
+    err
+}
+
+#[no_mangle]
+pub extern "C" fn stop(id: c_short) -> c_short {
+    let err: c_short;
+    unsafe {
+        err = operation::TUSB0216AD_Stop(id);
+        helper::parse_error(err, "TUSB0216AD_Stop");
+    }
+
+    err
+}
+
+fn status() -> (u8, c_uint, c_uint) {
+    let mut status = 1 as u8;
+    let mut overflow = [0, 0];
+    let mut datalen = [0, 0];
+    unsafe {
+        operation::TUSB0216AD_Ad_Status(
+            0,
+            &mut status as *mut u8,
+            overflow.as_mut_ptr(),
+            datalen.as_mut_ptr(),
+        );
+    }
+
+    println!("================");
+    println!("status: {}", status);
+    println!("Overflow: {:?}", overflow);
+    println!("DataLen: {:?}", datalen);
+    println!("================");
+
+    (status, datalen[0], datalen[1])
+}
+
+#[no_mangle]
+pub extern "C" fn trigger(id: c_short) -> c_short {
+    let err: c_short;
+    unsafe {
+        err = operation::TUSB0216AD_Trigger(id);
+        helper::parse_error(err, "TUSB0216AD_Trigger");
+    }
+
+    err
+}
+
+#[no_mangle]
+pub extern "C" fn single_data(id: c_short) {
+    let mut data = [10, 10];
+    unsafe {
+        let err = operation::TUSB0216AD_Ad_Single(id, data.as_mut_ptr());
+        helper::parse_error(err, "TUSB0216AD_Ad_Single");
+    }
+
+    println!("{:?}", data);
+}
+
+#[no_mangle]
+pub extern "C" fn run(id: c_short, seconds: u64) {
     // sequence が走っているかを示すフラグ
     // -1: not-started, 0: running, 1: finished
     let flag = Arc::new(Mutex::new(0));
@@ -92,4 +176,52 @@ pub extern "C" fn run(id: i32, seconds: u64) {
     time_keeper.join().expect("Paniced at time_keeper");
     job_runner.join().expect("Paniced at job_runner");
     post_data.join().expect("Paniced at post_data thread");
+}
+
+#[no_mangle]
+pub extern "C" fn test_run() {
+    const MAX_LENGTH: usize = 100000;
+    let mut length: c_uint;
+
+    let mut store: Vec<c_int> = vec![];
+
+    let mut err: c_short;
+    unsafe {
+        open(0);
+        set_clock(0, 500, 0);
+        input_set(0, 0, 0);
+        start(0, 0, 0, 0, 0);
+        trigger(0);
+
+        for _ in 0..100 {
+            let mut data1 = [0 as c_int; MAX_LENGTH];
+            let (status, ch1_len, _) = status();
+            length = ch1_len;
+
+            if status == 3 {
+                err = operation::TUSB0216AD_Ad_Data(
+                    0,
+                    0,
+                    data1.as_mut_ptr(),
+                    &mut length as *mut u32,
+                );
+                helper::parse_error(err, "TUSB0216AD_Ad_Data");
+            }
+
+            for i in 0..length as usize {
+                store.push(data1[i]);
+            }
+            println!("length: {}", length);
+        }
+
+        stop(0);
+        close(0);
+    }
+
+    let mut a: Vec<f32> = vec![];
+    for i in 0..store.len() as usize {
+        let result = helper::convert_to_voltage(0, 0, store[i] as f32, store[i] as f32);
+        a.push(result.0);
+    }
+    helper::write_to_csv("C:/Users/yudai/Desktop/a.csv", &a, &a);
 }
